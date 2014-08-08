8fad2c3a0b0ff758feaed07ce67fe1904c6ce454
Clint Walker <clint@neonrelic.com>
2014-08-08 15:53:07 -0400

    Add exports.default() to return default values if present

--- src/_property.js
+++ src/_property.js
@@ -23,3 +23,29 @@
 
 var Cast = require('./cast');
 var Rules = require('./rules');
+
+
+/**
+  Export this module
+*/
+
+module.exports = exports;
+
+
+/**
+  Returns the default value in the schema if no `value` present
+
+  @param {Mixed} v The value to default
+  @param {Schema} schema The associated schema to check the default value on
+
+  @return Value or the default value
+*/
+
+exports.default = function (v, schema) {
+  // No default, return the value as is
+  if (!schema || !schema.default) return v;
+
+  // Return the default if `v` is empty (ie. undefined or '')
+  return Rules.empty(v) ? schema.default : v;
+};
+
--- test/rebuild.test.js
+++ test/rebuild.test.js
@@ -3,3 +3,22 @@
 var expect = require('chai').expect
   , Schema = require('../src/schema')
   , schema = require('../src/_property');
+
+
+describe('default', function () {
+
+  it('returns the default value if no val provided', function () {
+    expect( schema.default( '', {default:'yes'})).to.equal('yes');
+  });
+
+  it('returns the passed value if provided', function () {
+    expect( schema.default('hi', {default:'yes'})).to.equal('hi');
+  });
+
+  it('returns the value if no Schema.default', function () {
+    expect( schema.default( '' ) ).to.equal('');
+    expect( schema.default('hi') ).to.equal('hi');
+  });
+
+});
+
