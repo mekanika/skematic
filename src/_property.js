d47d9fec2764f0660ba29b7d78cf5c7eb50e12bb
Clint Walker <clint@neonrelic.com>
2014-08-08 16:11:46 -0400

    Add exports.filter() to apply modifiers and casting

--- src/_property.js
+++ src/_property.js
@@ -49,3 +49,53 @@ exports.default = function (v, schema) {
   return Rules.empty(v) ? schema.default : v;
 };
 
+
+/**
+  Available filter functions (modifiers for value)
+*/
+
+var _filters = {
+  trim: function(v) { return v.trim(); },
+  uppercase: function (v) { return v.toUpperCase(); },
+  lowercase: function (v) { return v.toLowerCase(); }
+};
+
+// Add 'to$CAST' casters to filters
+for (var cast in Cast) if (cast.substr(0,2)==='to') _filters[cast] = Cast[cast];
+
+
+/**
+  Applies a list of filter functions
+
+  @param {Mixed} val The value to filter
+  @param {String[]} filters Array of named filters to apply
+
+  @throws {Error} if filter cannot be applied
+*/
+
+exports.filter = function (val, filters) {
+  // Ensure filters are provided as an array
+  if (typeof filters === 'string') filters = [filters];
+
+  filters.forEach( function (key) {
+    try {
+      if ( _filters[key] ) val = _filters[key]( val );
+    }
+    catch( e ) {
+      console.log('Cannot apply filter: '+key);
+      throw e;
+    }
+  });
+
+  return val;
+};
+
+
+/**
+  Exposes the available filters as an array
+
+  @type Array
+*/
+
+exports.filter.available = Object.keys(_filters);
+
--- test/rebuild.test.js
+++ test/rebuild.test.js
@@ -2,6 +2,7 @@
 
 var expect = require('chai').expect
   , Schema = require('../src/schema')
+  , Cast = require('../src/cast')
   , schema = require('../src/_property');
 
 
@@ -18,6 +19,52 @@ describe('default', function () {
   it('returns the value if no Schema.default', function () {
     expect( schema.default( '' ) ).to.equal('');
     expect( schema.default('hi') ).to.equal('hi');
+  });
+
+});
+
+
+describe('filters', function () {
+
+  describe('apply', function () {
+    it('applies array of filter keys', function () {
+      expect( schema.filter(' ! ', ['trim']) ).to.equal( '!' );
+    });
+
+    it('ignores unknown filter types', function () {
+      expect( schema.filter(' ! ', ['hodown']) ).to.equal( ' ! ' );
+    });
+
+    it('throws if a filter cannot apply', function () {
+      var err;
+      try { schema.filter( NaN, ['trim'] ); }
+      catch (e) { err = e; }
+      expect( err ).to.be.an.instanceof( Error );
+    });
+  });
+
+  it('.availabe provides list of available filters', function () {
+    expect( schema.filter.available ).to.have.length.gt( 0 );
+  });
+
+  it('to$Cast available filters [see cast tests]', function () {
+    var keys = Object.keys( Cast );
+    keys.forEach( function (key) {
+      if (key.substr(0,2) === 'to')
+        expect( schema.filter.available.indexOf(key) ).to.be.gt(-1);
+    });
+  });
+
+  it('"trim" strings', function () {
+    expect( schema.filter( ' ! .. 2 ', ['trim']) ).to.equal( '! .. 2' );
+  });
+
+  it('"uppercase" strings', function () {
+    expect( schema.filter( 'abc', ['uppercase']) ).to.equal( 'ABC' );
+  });
+
+  it('"lowercase" strings', function () {
+    expect( schema.filter( 'AbC', ['lowercase']) ).to.equal( 'abc' );
   });
 
 });
