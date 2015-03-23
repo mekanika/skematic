f726d0590f2f8e83120bcfc8e5e4272002c6fd68
Clint Walker <cw@neonrelic.com>
2015-03-23 14:22:38 +0800

    Add ‘strip(values, data)’ method

---
+++ test/strip.test.js
@@ -0,0 +1,20 @@
+
+var expect = require('chai').expect
+  , strip = require('../lib/strip');
+
+
+describe('.strip()', function () {
+
+  it('removes fields whose values match', function (){
+    var data = {a:undefined, b:2};
+    strip( undefined, data );
+    expect( data ).to.eql({b:2});
+  });
+
+  it('allows passing arrays of `values`', function () {
+    var data = {a:undefined, b:null, c:2, d:':)'};
+    strip( [undefined, null, ':)'], data);
+    expect( data ).to.eql( {c:2} );
+  });
+
+});
