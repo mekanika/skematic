a2302f5aa6400b7eb232a9891e706c0e9c0f24e5
Clint Walker <cw@neonrelic.com>
2015-03-31 18:23:23 +0800

    Add `.idMap()` primary key map utility #17

---
+++ test/idmap.test.js
@@ -0,0 +1,38 @@
+
+var expect = require('chai').expect
+  , clone = require('mekanika-lsd/clone')
+  , idMap = require('../lib/idmap');
+
+describe('idMap()', function () {
+
+  var propSchema = {
+    prop_id: {primaryKey:true},
+    name: {type:"string"}
+  };
+
+  var data = [ {_id:"512314", name:"power"}, {_id:"519910", name:"speed"} ];
+
+  it('transposes to a primaryKey field from an idField', function () {
+
+    var out = idMap( propSchema, clone(data), '_id' );
+
+    expect( out ).to.have.length(2);
+    expect( out[0] ).to.include.key( 'prop_id' );
+    expect( out[0] ).to.not.include.key( '_id' );
+
+  });
+
+  it('does nothing if no primaryKey defined', function () {
+    var ps = {name:{type:'string'}};
+    var out = idMap( ps, data, '_id' );
+
+    expect( out ).to.eql( data );
+  });
+
+  it('does nothing if primarKey field has a `generate` field', function () {
+    var ps = {prop_id:{primaryKey:true, generate:{ops:[Math.random]}}};
+    var out = idMap( ps, data, '_id' );
+    expect( out ).to.eql( data );
+  });
+
+});
