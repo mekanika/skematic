97481f1dab0e75ceade783d52bf79022ecabf58a
Clint Walker <cw@neonrelic.com>
2016-07-28 23:30:19 -0300

    Add Enumerated Type support to toSQL()

--- src/plugins/toSQL.js
+++ src/plugins/toSQL.js
@@ -5,14 +5,14 @@
 */
 
 export const pgSQL = {
-  ARRAY: t => typeToSQL(t.type) + ' ARRAY',
+  ARRAY: (t, opts) => typeToSQL(t.type, opts) + ' ARRAY',
   BOOLEAN: t => t.key,
   BYTEA: t => t.key,
   CHAR: t => 'CHAR' + (t.length ? `(${t.length})` : ''),
   DATE: t => 'TIMESTAMPTZ',
   DECIMAL: t => t.key,
   DOUBLE: t => t.key,
-  ENUM: t => t.key,
+  ENUM: (t, opts) => opts.fieldName,
   FLOAT: t => t.key,
   GEOGRAPHY: t => t.key,
   GEOMETRY: t => t.key,
@@ -49,20 +49,34 @@ export const pgSQL = {
   @return {String} The SQL compliant string
 */
 
-export const typeToSQL = (type, forceUnrecognised = false) => {
+export const typeToSQL = (type, opts = {forceUnrecognised: false}) => {
   if (!type) return ''
 
   // Return immediately if no key set on data type
   if (!type.key) {
-    if (forceUnrecognised) return type
+    if (opts.forceUnrecognised) return type
     throw new Error('Unrecognised type: ' + type)
   }
 
-  if (pgSQL[type.key]) return pgSQL[type.key](type)
+  if (pgSQL[type.key]) return pgSQL[type.key](type, opts)
   else {
-    if (forceUnrecognised) return type.key
+    if (opts.forceUnrecognised) return type.key
     throw new Error('Unrecognised type: ' + type.key)
   }
+}
+
+/**
+  Converts a JS array to a PG SQL compatible string
+  @example
+  arrayToPGString(['1', '2'])
+  // -> '{"1", "2"}'
+
+  @param {Array} arr The array to convert
+  @return {String}
+*/
+
+export const arrayToPGString = arr => {
+  return JSON.stringify(arr).replace(/\[/g, '{').replace(/\]/g, '}')
 }
 
 /**
@@ -84,10 +98,28 @@ export const fieldToString = (name, model, table) => {
   }
 
   // Specify the column type
-  str.push(typeToSQL(model.type, true))
+  str.push(typeToSQL(model.type, {
+    forceUnrecognised: true,
+    fieldName: name
+  }))
 
   // Apply any defaults
-  if (model.default) str.push('DEFAULT ' + JSON.stringify(model.default))
+  // Note that PG requires strings to use `'` single quotes and arrays to be
+  // formatted as `{val1, val2}` rather than '[val1, val2]'
+  if (model.default) {
+    let def = model.default
+
+    if (Array.isArray(model.default)) {
+      def = arrayToPGString(model.default)
+    } else if (typeof model.default === 'object') {
+      def = JSON.stringify(model.default)
+    }
+
+    // Ensure all string values of `def` are single quoted
+    if (typeof def === 'string') def = `'${def}'`
+
+    str.push(`DEFAULT ${def}`)
+  }
 
   // Specify Primary key attribute
   if (model.primaryKey) str.push('PRIMARY KEY')
@@ -96,6 +128,27 @@ export const fieldToString = (name, model, table) => {
   if (model.allowNull === false) str.push('NOT NULL')
 
   return str.join(' ')
+}
+
+/**
+  Generates a `CREATE TYPE` enumerated string for all enum fields in `model`
+
+  @param {Object} model The Skematic model with potential enum fields
+  @return {String[]|null} Array of CREATE TYPE strings or null if none
+*/
+
+const getEnumTypeString = model => {
+  let enums = []
+
+  Object.keys(model).forEach(key => {
+    const type = model[key].type
+    if (type.key !== 'ENUM') return
+
+    let ts = type.values.map(v => typeof v === 'string' ? `'${v}'` : v)
+    enums.push(`CREATE TYPE ${key} AS ENUM (${ts.join(', ')});`)
+  })
+
+  return enums.length ? enums : null
 }
 
 /**
@@ -128,6 +181,10 @@ export default function toSQL (schema, opts = {}) {
     if (true) { // opts.dropTables)
       str += `\nDROP TABLE IF EXISTS ${k};`
     }
+
+    // Generate ENUM types if present
+    const enums = getEnumTypeString(model)
+    if (enums) str += '\n' + enums.join('\n')
 
     str += `\nCREATE TABLE ${k} (\n`
     let fields = Object.keys(model).map(field =>
