64f11e150a818ac1eed04bfd1a5ca30f25f15899
Clint Walker <cw@neonrelic.com>
2018-03-21 13:05:49 +0200

    docs: reformat type comments for intellisense

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,80 +3,112 @@ export as namespace Skematic;
 
 declare namespace Skematic {
   interface FormatOptions {
-    // defaults
-    // {Boolean} - true
-    // Set default values on 'empty' fields. Toggle to false to disable.
+    /**
+     * defaults:
+     * Set default values on 'empty' fields. Toggle to false to disable.
+     * @type {Boolean}
+     */
     defaults?: boolean;
-    // scopes
-    // {String|Array} - undefined
-    // List of scopes that toggle .show model fields on format()
-    // (See validate() for .write scopes)
+    /**
+     * scopes:
+     * List of scopes that toggle .show model fields on format()
+     * See: validate() for .write scopes)
+     * @type {String|String[]}
+     */
     scopes?: string | string[];
-    // unscope
-    // {Boolean} - false
-    // Ignores 'show' of scopes (ie. shows all fields)
+    /**
+     * unscope:
+     * Ignores 'show' of scopes (ie. shows all fields)
+     * @type {Boolean}
+     */
     unscope?: boolean;
-    // strict
-    // {Boolean} - false
-    // Strips any fields not declared on model
+    /**
+     * strict:
+     * Strips any fields not declared on model
+     * @type {Boolean}
+     */
     strict?: boolean;
-    // sparse
-    // {Boolean} - false
-    // Only process fields on the provided data, rather than all
-    // fields on the entire model
+    /**
+     * sparse:
+     * Only process fields on the provided data, rather than all
+     * fields on the entire model
+     * @type {Boolean}
+     */
     sparse?: boolean;
-    // generate
-    // {Boolean} - true
-    // Enable/disable generating new values - see Design:generate
+    /**
+     * generate:
+     * Enable/disable generating new values - see Design:generate
+     * @type {Boolean}
+     */
     generate?: boolean;
-    // once
-    // {Boolean} - false
-    // Run generator functions set to {once: true} - see Design:generate
+    /**
+     * once:
+     * Run generator functions set to {once: true} - see Design:generate
+     * @type {Boolean}
+     */
     once?: boolean;
-    // transform
-    // {Boolean} - true
-    // Toggle to false to cancel modifying values
+    /**
+     * transform:
+     * Toggle to false to cancel modifying values
+     * @type {Boolean}
+     */
     transform?: boolean;
-    // unlock
-    // {Boolean} - false
-    // Unlocks 'lock'ed model fields (ie. no longer stripped,
-    // allows for overwriting).
+    /**
+     * unlock:
+     * Unlocks 'lock'ed model fields (ie. no longer stripped,
+     * allows for overwriting).
+     * @type {Boolean}
+     */
     unlock?: boolean;
-    // strip
-    // {Array} - []
-    // Remove fields with matching values from data
+    /**
+     * strip
+     * Remove fields with matching values from data
+     * @type {any[]}
+     */
     strip?: any[];
-    // mapIdFrom
-    // {String} - undefined
-    // Maps a primary key field from the field name provided
-    // (requires a primaryKey field set on the model)
+    /**
+     * mapIdFrom:
+     * Maps a primary key field from the field name provided
+     * (requires a primaryKey field set on the model)
+     * @type {String}
+     */
     mapIdFrom?: string;
   }
 
   interface ValidateOptions {
-    // scopes
-    // {String|Array} - undefined
-    // List of scopes that will be tested against .write model
-    // fields for matches. Errors if scopes don't meet.
+    /**
+     * scopes:
+     * List of scopes that will be tested against .write model
+     * fields for matches. Errors if scopes don't meet.
+     * @type {String|String[]}
+     */
     scopes?: string | string[];
-    // unscope
-    // {Boolean} - false
-    // Ignores any .write scope requirements on the model
+    /**
+     * unscope:
+     * Ignores any .write scope requirements on the model
+     * @type {Boolean}
+     */
     unscope?: boolean;
-    // strict
-    // {Boolean} - false
-    // Validates that all keys provided by data are defined on the model
-    // as well as valid (prevents validating/accepting extraneous fields)
+    /**
+     * strict:
+     * Validates that all keys provided by data are defined on the model
+     * as well as valid (prevents validating/accepting extraneous fields)
+     * @type {Boolean}
+     */
     strict?: boolean;
-    // sparse
-    // {Boolean} - false
-    // Only process fields on the provided data, rather than all
-    // fields on the entire model
+    /**
+     * sparse:
+     * Only process fields on the provided data, rather than all
+     * fields on the entire model
+     * @type {Boolean}
+     */
     sparse?: boolean;
-    // keyCheckOnly
-    // {Boolean} - false
-    // Overrides normal validation and ONLY checks user data keys are all
-    // defined on model. Useful to ensure user is not sending bogus keys.
+    /**
+     * keyCheckOnly:
+     * Overrides normal validation and ONLY checks user data keys are all
+     * defined on model. Useful to ensure user is not sending bogus keys.
+     * @type {Boolean}
+     */
     keyCheckOnly?: boolean;
   }
 
@@ -86,64 +118,73 @@ declare namespace Skematic {
   type RuleMethod = (value?: any) => boolean;
 
   interface Rules {
-    // Allow for custom rules - even though we can't specify its type as
-    // RuleMethod due to the inability to mix Index Signatures
+    /**
+     * Allow for custom rules - even though we can't specify its type as
+     * RuleMethod due to the inability to mix Index Signatures
+     */
     [custom: string]: any; // RuleMethod
-    // The lowest permitted number
+    /** The lowest permitted number */
     min?: number;
-    // The highest permitted number
+    /** The highest permitted number */
     max?: number;
-    // The shortest string permitted
+    /** The shortest string permitted */
     minLength?: number;
-    // The longest string permitted
+    /** The longest string permitted */
     maxLength?: number;
-    // Value must be strictly equal
+    /** Value must be strictly equal */
     eq?: any;
-    // Value must not equal
+    /** Value must not equal */
     neq?: any;
-    // Value must be one of the values in the list of elements
+    /** Value must be one of the values in the list of elements */
     oneOf?: any[];
-    // Value must NOT be in the list of elements
+    /** Value must NOT be in the list of elements */
     notOneOf?: any[];
-    // List of elements contains the value
+    /** List of elements contains the value */
     has?: any;
-    // List of elements does NOT contain the value
+    /** List of elements does NOT contain the value */
     hasNot?: any;
-    // no parameters: Is the string an email
+    /** no parameters: Is the string an email */
     isEmail?: boolean;
-    // no parameters: Is the string a URL
+    /** no parameters: Is the string a URL */
     isUrl?: boolean;
-    // no parameters: Checks value is an ALPHA string (abcd...)
+    /** no parameters: Checks value is an ALPHA string (abcd...) */
     isAlpha?: boolean;
-    // no parameters: Checks value is AlphaNumeric (abc..012..9)
+    /** no parameters: Checks value is AlphaNumeric (abc..012..9) */
     isAlphaNum?: boolean;
-    // no parameters: Checks value is a Number (NaN fails this test)
+    /** no parameters: Checks value is a Number (NaN fails this test) */
     isNumber?: boolean;
-    // no parameters: Checks value is of type String
+    /** no parameters: Checks value is of type String */
     isString?: boolean;
-    // String must match regexp
+    /** String must match regexp */
     match?: RegExp;
-    // String must NOT match regexp
+    /** String must NOT match regexp */
     notMatch?: RegExp;
-    // set to true to check the value is empty
+    /** set to true to check the value is empty */
     isEmpty?: boolean;
-    // set to true to check the value is not empty
+    /** set to true to check the value is not empty */
     notEmpty?: boolean;
   }
 
-  // Arbitrary function
+  /**
+   * Arbitrary function
+   */
   type Fn = (v?: any) => any;
 
+  /**
+   * Skematic Generators return values pending certain options passed as params
+   */
   interface Generator {
-      // The ops array lists fn objects or functions
-      // A fn object specifies `fn` and `args`:
-      // -> {fn: myFunc, args: []}
+      /**
+       * The ops array lists fn objects or functions
+       * A fn object specifies `fn` and `args`:
+       * -> {fn: myFunc, args: []}
+       */
       ops: Array<{fn: Fn, args: any[]} | Fn> | Fn;
-      // preserves a provided value
+      /** preserves a provided value */
       preserve?: boolean;
-      // ONLY generate if provided a field on data
+      /** ONLY generate if provided a field on data */
       require?: boolean;
-      // Require passing {once:true} to format to compute value
+      /** Require passing {once:true} to format to compute value */
       once?: boolean;
   }
 
@@ -152,51 +193,86 @@ declare namespace Skematic {
   }
 
   interface ModelProps {
-    // default {any}
-    // value to apply if no value is set/passed
+    /**
+     * default:
+     * Value to apply if no value is set/passed
+     * @type {any}
+     */
     default?: any;
-    // lock {Boolean}
-    // disallows/strips value on format (unlock format opts to override)
+    /**
+     * lock:
+     * Disallows/strips value on format (unlock format opts to override)
+     * @type {Boolean}
+     */
     lock?: boolean;
-    // transform {Function}
-    // a function to transform a value (not called if value is
-    // undefined or null)
+    /**
+     * transform:
+     * Function to transform a value (not called if value is
+     * @type {Function}
+     */
     transform?: (value: any) => any;
-    // generate {Object|Function}
-    // enables computing a value from functions
+    /**
+     * generate:
+     * Enables computing a value from functions
+     * @type {Generator|Fn}
+     */
     generate?: Generator | Fn;
-    // show {String|Array}
-    // string scopes required to show field on format (hides if not met)
+    /**
+     * show:
+     * Scopes required to show field on format (hides if not met)
+     * @type {String|String[]}
+     */
     show?: string | string[];
-    // write {String|Array}
-    // scopes required to validate this field being set (fails validation if
-    // scopes aren't matching)
+    /**
+     * write:
+     * @type {String|String[]}
+     * Scopes required to validate this field being set (fails validation if
+     * scopes aren't matching)
+     */
     write?: string | string[];
-    // model {Object}
-    // declare sub-model defining this value (see "Sub-model")
+    /**
+     * model
+     * declare sub-model defining this value (see "Sub-model")
+     * @type {Skematic.Model}
+     */
     model?: Model;
-    // rules {Object}
-    // validation rules: {rules: {min: 3, max: 11}}
+    /**
+     * rules:
+     * Validation rules: `{rules: {min: 3, max: 11}}`
+     * @type {Skematic.Rules}
+     */
     rules?: Rules;
-    // errors {Object|String}
-    // error messages for rules
+    /**
+     * errors:
+     * Error messages for rules
+     * @type {Object|String}
+     */
     errors?: object | string;
-    // required {Boolean}
-    // flag if property MUST be set and/or provided
+    /**
+     * required:
+     * Flag if property MUST be set and/or provided
+     * @type {Boolean}
+     */
     required?: boolean;
-    // allowNull {Boolean}
-    // Accept null values (no other validation applied) or set to false to
-    // force a NOT NULL condition (no undefined or null values permitted).
-    // Designed to: a) false: enables setting required (which ordinarily
-    // passes for null) while disallowing null as a value. b) true: enables
-    // accepting null without triggering any other rule validation (ie. 'null'
-    // becomes a valid value)
+    /**
+     * allowNull:
+     * Accept null values (no other validation applied) or set to false to
+     * force a NOT NULL condition (no undefined or null values permitted).
+     * Designed to: a) false: enables setting required (which ordinarily
+     * passes for null) while disallowing null as a value. b) true: enables
+     * accepting null without triggering any other rule validation (ie. 'null'
+     * becomes a valid value)
+     * @type {Boolean}
+     */
     allowNull?: boolean;
-    // primaryKey {Boolean}
-    // flag to indicate whether this field is the primary key (id field),
-    // used in conjunction with mapIdFrom format option to allow transposing
-    // your datastore id to some other field on your data model (eg. Mongo's
-    // _id can be mapped to the field you set primaryKey: true on)
+    /**
+     * primaryKey:
+     * flag to indicate whether this field is the primary key (id field),
+     * used in conjunction with mapIdFrom format option to allow transposing
+     * your datastore id to some other field on your data model (eg. Mongo's
+     * _id can be mapped to the field you set primaryKey: true on)
+     * @type {Boolean}
+     */
     primaryKey?: boolean;
   }
 
@@ -210,18 +286,36 @@ declare namespace Skematic {
     errors: string[] | null;
   }
 
+  /**
+   * Formats incoming `data` against a provided model
+   * @param {Skematic.Model|Skematic.ModelProps} model
+   * @param {any} data
+   * @param {Skematic.FormatOptions} options
+   */
   function format(
     model: Model | ModelProps,
     data?: any,
     options?: FormatOptions
   ): any;
 
+  /**
+   * Moo
+   * @param {Skematic.Model|Skematic.ModelProps} model
+   * @param {any} data
+   * @param {Skematic.ValidateOptions} options
+   */
   function validate(
     model: Model,
     data: any,
     options?: ValidateOptions
   ): ValidateComplexReturn;
 
+  /**
+   * Hello
+   * @param {Skematic.ModelProps} model
+   * @param data
+   * @param options
+   */
   function validate(
     model: ModelProps,
     data: any,
